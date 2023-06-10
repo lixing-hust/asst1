c91005ef8bf7372489cc52bac7d4301c99ce74c3
lixing-hust <1291729176@qq.com>
2023-06-10 12:38:03 +0800

    finishi peog4

--- prog4_sqrt/main.cpp
+++ prog4_sqrt/main.cpp
@@ -2,6 +2,7 @@
 #include <algorithm>
 #include <pthread.h>
 #include <math.h>
+#include <immintrin.h>
 
 #include "CycleTimer.h"
 #include "sqrt_ispc.h"
@@ -17,6 +18,52 @@ static void verifyResult(int N, float* result, float* gold) {
         }
     }
 }
+
+void sqrtAVX(int N,
+                float initialGuess,
+                float values[],
+                float output[])
+{
+
+    static const float kThreshold = 0.00001f;
+    __mmask8 mask;
+    __m256 tmp,left,right; 
+    __m256 ONE = _mm256_set1_ps(1.f);
+    __m256 PointFive = _mm256_set1_ps(0.5f);
+    __m256 THREE = _mm256_set1_ps(3.f);
+    __m256 ABS = _mm256_castsi256_ps(_mm256_set1_epi32(0x7fffffff));
+    __m256 vector_Threshold = _mm256_set1_ps(kThreshold);
+
+    for (int i=0; i<N; i+=8) {
+        mask=0xff;
+        __m256 origx = _mm256_load_ps(&values[i]);
+        __m256 vector_guess = _mm256_set1_ps(initialGuess);
+        
+        tmp = _mm256_mask_mul_ps(vector_guess,mask,vector_guess,vector_guess);
+        tmp = _mm256_mask_mul_ps(tmp,mask,tmp,origx);
+        tmp = _mm256_mask_sub_ps(tmp,mask,tmp,ONE);
+        tmp = _mm256_mask_and_ps(tmp,mask,tmp,ABS);
+        mask = mask & (_mm256_cmp_ps_mask(tmp,vector_Threshold,(unsigned char)30));
+
+        while(mask){
+            left = _mm256_mask_mul_ps(vector_guess,mask,vector_guess,THREE);
+            right = _mm256_mask_mul_ps(vector_guess,mask,vector_guess,vector_guess);
+            right = _mm256_mask_mul_ps(right,mask,right,vector_guess);
+            right = _mm256_mask_mul_ps(right,mask,origx,right);
+            left = _mm256_mask_sub_ps(left,mask,left,right);
+            vector_guess = _mm256_mask_mul_ps(left,mask,left,PointFive);
+
+            tmp = _mm256_mask_mul_ps(vector_guess,mask,vector_guess,vector_guess);
+            tmp = _mm256_mask_mul_ps(tmp,mask,tmp,origx);
+            tmp = _mm256_mask_sub_ps(tmp,mask,tmp,ONE);
+            tmp = _mm256_mask_and_ps(tmp,mask,tmp,ABS);
+            mask = mask & (_mm256_cmp_ps_mask(tmp,vector_Threshold,(unsigned char)30));
+        }
+
+        _mm256_store_ps(&output[i],vector_guess);
+    }
+}
+
 
 int main() {
 
@@ -34,7 +81,9 @@ int main() {
         // to you generate best and worse-case speedups
         
         // starter code populates array with random input values
-        values[i] = .001f + 2.998f * static_cast<float>(rand()) / RAND_MAX;
+        values[i] = 0.001f + 2.998f * static_cast<float>(rand()) / RAND_MAX;
+        // for(unsigned int j= i+1;j<i+8;j++)
+        //     values[j]=1.f;
     }
 
     // generate a gold version to check results
@@ -92,8 +141,25 @@ int main() {
 
     verifyResult(N, output, gold);
 
+    // Clear out the buffer
+    for (unsigned int i = 0; i < N; ++i)
+        output[i] = 0;
+    
+    double minAVX = 1e30;
+    for (int i = 0; i < 3; ++i) {
+        double startTime = CycleTimer::currentSeconds();
+        sqrtAVX(N, initialGuess, values, output);
+        double endTime = CycleTimer::currentSeconds();
+        minTaskISPC = std::min(minAVX, endTime - startTime);
+    }
+
+    printf("[sqrt AVX]:\t[%.3f] ms\n", minAVX * 1000);
+
+    verifyResult(N, output, gold);
+
     printf("\t\t\t\t(%.2fx speedup from ISPC)\n", minSerial/minISPC);
     printf("\t\t\t\t(%.2fx speedup from task ISPC)\n", minSerial/minTaskISPC);
+    printf("\t\t\t\t(%.2fx speedup from AVX)\n", minSerial/minAVX);
 
     delete [] values;
     delete [] output;
